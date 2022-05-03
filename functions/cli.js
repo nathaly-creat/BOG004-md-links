7ab04b7cc4c9b45cc041fda415dbf26db90a9692
Nathaly Huerta <nathaly.huertab@gmail.com>
2022-05-03 11:47:47 -0500

    funcinalidad de mdlinks-sin options

--- functions/cli.js
+++ functions/cli.js
@@ -5,23 +5,16 @@ const mdLinks = require('./index.js');
 const process = require('process');
 const stats = require ('./stats.js');
 const validate = require ('./validate.js');
+const { resolve } = require('path');
 
-
-/*Definir metodos de node para definir el argumento de la función*/
 
 const imputRoute = process.argv[2];
 const options = process.argv[3];
 
-// console.log('hola');
-mdLinks(imputRoute).then ((links) => {
+mdLinks(imputRoute, options).then ((links) => {
+    console.log('linkkkkkks', links);
+    // console.log('linksdeCLI', links);
     // console.log('links2', links);
-    // stats(links);
-    // validate(links);
+//   stats(links);
+//   validate(links);
 });
-
-// .then(response => console.log('response', response))
-// .catch(error => console.log('error', error));
-
-
-// aqui debo construir la funcion validate
-// cosntruir condicional ara los csos de si exsite o no en la path validate y el siguiente es de stats
--- functions/getFiles.js
+++ functions/getFiles.js
@@ -5,76 +5,84 @@ const fs = require('fs');
 const isFileMd = (pathAbs) => path.extname(pathAbs) === ".md";
 const isDirectory = (pathAbs) => fs.lstatSync(pathAbs).isDirectory();
 
+
 // /*MODELO 1 FUNCION DE RECURSIVIDAD*/
-const getFiles = (pathAbs, globalFiles) => new Promise((resolve) => {
-        let arrFiles = [];
-    const files = fs.readdirSync(pathAbs);  
-    files.forEach(file => {
-        const filePath = path.join(pathAbs, file); 
-        console.log('filePath', filePath);
-        if (isFileMd(filePath)) {
-          arrFiles.push(filePath);
-        } else if (isDirectory(filePath)) {
-          getFiles(filePath , globalFiles);
-        }
-        });
-        Promise.all(arrFiles).then((files) => {
-          globalFiles = files;
-          if (globalFiles.length > 0) {
-            // console.log('globalFiles', globalFiles);
-            getLinksFiles(globalFiles);
-          }
-          // resolve(files);
-          resolve(globalFiles);
-        });
-        
-    }); 
-    
+
+  // let globalFiles = [];
+  const getFiles = (pathAbs, globalFiles) => {
+  const files = fs.readdirSync(pathAbs);  
+  files.forEach(file => {
+    const filePath = path.join(pathAbs, file); 
+    // console.log('filePath', filePath);
+    if(isDirectory(filePath)) {
+      getFiles(filePath, globalFiles);
+    } else {
+      if (isFileMd(filePath)) {
+      globalFiles.push(filePath);
+    } 
+  }
+    });
+  }; 
+
+    // Promise.all(arrFiles).then((files) => {
+    //   // console.log('filesB', files);
+    //   globalFiles = files;
+    //   if (globalFiles.length > 0) {
+    //     // console.log('globalFiles', globalFiles);
+    //     getLinksFiles(globalFiles);
+    //   }
+    //   // resolve(files);
+    //   resolve(globalFiles);
+    // });
     
 
+
+
     // /* ---------- Obtener links archivo .md ---------- */
-const getLinksMd = (pathAbs) => new Promise((resolve, reject) => {
-    // pathAbs = pathAbs.replace(/\\/g, '/');
-    let fileLinks = pathAbs;
-    fs.readFile(fileLinks, 'utf-8', (error, data = [...globalFiles(fileLinks)] ) => {
-      // console.log('data', data);
-      const regularExpression = /\[([^\[]+)\](\(.*\))/g;
-    //   const internalLinks = data.match(regularExpression);
-      // console.log('links encontrados INTERNAL: ', internalLinks);
-      if (error) {
-        reject(new Error(`${error.code}, ' ❌ Error de lectura del archivo ${file}'`));
-      } else if (data.match(regularExpression) !== null && data !== '' && data !== undefined) {
-        const links = data.match(regularExpression);
-        const arrayLinksStructure = links.map(link => {
-          const linkName = link.match(/\[.*\]/)[0].replace(/\[|\]/g, '');
-          const linkUrl = link.match(/\(.*\)/)[0].replace(/\(|\)/g, '');
-          return {
-            text: linkName,
-            url: linkUrl,
-            file: fileLinks,
-          };
+    const getLinksMd = (pathAbs) => { return new Promise((resolve, reject) => {
+      let fileLinks = pathAbs;
+      fs.readFile(fileLinks, 'utf8', (err, data) => {
+        if (err) {
+          console.log('❌ No es un archivo.md o un directorio');
+          reject(err);
+        } else {
+          let links = data.match(/\[(.*?)\]\((.*?)\)/g);
+          if (links !== null) {
+            let promesa = [];
+            links.forEach((objeto) => {
+              const linkName = objeto.match(/\[.*\]/)[0].replace(/\[|\]/g, '');
+              const linkUrl = objeto.match(/\(.*\)/)[0].replace(/\(|\)/g, '');             
+              let linkObj = {
+                text: linkName,
+                href: linkUrl,
+                file: fileLinks,
+              };
+              promesa.push(linkObj);
+              });
+              Promise.all(promesa).then((links) => {
+                // console.log('links', links);
+                resolve(links);
+              });
+          } else {
+            reject(new Error(`${error.code}, ' ❌ Error de lectura del archivo ${file}'`));
+            resolve([]);
+          }
+          
+          }
         });
-        resolve([arrayLinksStructure]);
-        console.log('LinksDestructure', arrayLinksStructure);
-      } else {
-        resolve([]);
-      }
-    });
-  });
-  
-  /* ---------- opcion 1 Obtener links de array archivos .md ---------- */
+      });
+    };
+
+       
+      /* ---------- opcion 1 Obtener links de array archivos .md ---------- */
   // });
   
   
-    const getLinksFiles = (arrayPathAbs) => {
+  const getLinksFiles = (arrayPathAbs) => {
     const filesPromises = [];
-    arrayPathAbs.map((file) => {
-      filesPromises.push(getLinksMd(file));
-  
-      return Promise.all(filesPromises).then((arrayLinks) => {
-        // console.log('arrayLinks', arrayLinks);
-        return arrayLinks;
-      });
+    return arrayPathAbs.map((file) => {
+      return getLinksMd(file)
+     
     });
     
   };
